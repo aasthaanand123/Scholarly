fb26c90bbcefb2ffc6be97891d5a95f2e59c6165
Aastha <aasthaanand193@hotmail.com>
2024-04-08 23:28:21 +0530

    included headers in request sent

--- src/app/backend-service.service.ts
+++ src/app/backend-service.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
@@ -7,11 +7,16 @@ import { Observable } from 'rxjs';
 export class BackendServiceService {
   constructor(private http: HttpClient) {}
   url_template = 'http://localhost:3000/';
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
   signUp(data: any): Observable<any> {
-    return this.http.post(this.url_template + 'signup', JSON.stringify(data));
+    return this.http.post(this.url_template + 'sign-up', JSON.stringify(data), {
+      headers: this.headers,
+    });
   }
 
   signIn(data: any): Observable<any> {
-    return this.http.post(this.url_template + 'sign-in', JSON.stringify(data));
+    return this.http.post(this.url_template + 'sign-in', JSON.stringify(data), {
+      headers: this.headers,
+    });
   }
 }
